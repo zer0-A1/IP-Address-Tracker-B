593d3fe9932d10425511d3a48702b7953ccbaa58
zer0-A1 <lukasmetcalfe1111@gmail.com>
2023-04-20 07:05:48 +0330

    Added vercel.json

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -27,7 +27,6 @@ export const fetchDataFromApi = async (
   let url = "";
   if (api === "ipdata") url = API_URL[api].replace("query", ipAddress);
   else url = API_URL[api] + ipAddress;
-  console.log(url);
   try {
     const fetchRes = await fetch(url);
     if (!fetchRes.ok)
